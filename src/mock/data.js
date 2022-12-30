14477873b295eabb479836074c1e62a8bc074bef
Lauri Kyttälä <lauri.kyttala@gmail.com>
2022-12-30 18:00:48 +0200

    made own modifications to mock/data.js and image files

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,47 +11,50 @@ export const headData = {
 export const heroData = {
   title: 'Hello, my name is ',
   name: 'Lauri',
-  subtitle: "I 'm a software tester",
+  subtitle: "I 'm a software Tester / Developer",
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne:
+    'I am an ICT professional having experience in different technical environments like software development / testing, telecommunications and microchip testing. I have made documentations, as well as some modelling. I have participated in projects as a member or as the project manager.',
+  paragraphTwo:
+    'Lastly on ICT-sector I worked as a programmer / tester. I find the continuous learning through work and studying very important.',
+  paragraphThree:
+    'My work colleagues consider me as a systematic, and cooperative. I am used to teamwork being also initiative. I have experience in project management and customer service work.',
+  resume: 'https://www.linkedin.com/in/laurikyttala/', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'argentine-tango.jpg',
+    title: 'Dance Calendar',
+    info: 'Comment and save your favourite ballrooms, make calendar notes and much more',
+    info2: 'Nodejs, React, Redux, MongoDB',
+    url: 'https://dance-calendar.herokuapp.com/home',
+    repo: 'https://github.com/lasse1900/dance-calendar-test', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'girlsFace.jpg',
+    title: 'Facerecognition App',
+    info: 'AI face recognition',
+    info2: 'Nodejs, ReactJS, PostgreSQL',
+    url: 'https://facerecognition-lk.herokuapp.com/',
+    repo: 'https://github.com/lasse1900/facerecognition-app', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'LUT.jpg',
+    title: 'My Thesis',
+    info: 'The Development of Practical Programming Course at LUT-university',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://lutpub.lut.fi/handle/10024/72533',
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
@@ -68,22 +71,22 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/laurikyt',
     },
     {
       id: nanoid(),
       name: 'codepen',
-      url: '',
+      url: 'https://github.com/lasse1900',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/laurikyttala/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/lasse1900',
     },
   ],
 };
